e53d88d3a50bb4d42fa60ef98f8352926b5a2b75
Simone Capuano <scapuano19@gmail.com>
2021-03-07 11:31:40 +0100

    display products

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import data from './data';
+import Product from './components/Product';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+    return (
+      <div className="grid-container">
+        <header className="row">
+        <div>
+         <a className="brand" href="/">Iron Outlet</a>
+         </div>
+         <div>
+           <a href="/cart">Cart</a>
+           <a href="/signin">Sign in</a>
+         </div>
+        </header>
+        <main>
+        <div className="row center">
+        {
+          data.products.map(product => (
+           <Product key={product.id} product={product} />
+          ))
+        }
+        </div>
+        </main>
+        <footer className="row center">
+        All rights reserved!
+        </footer>
+      </div> 
+    );
+  }
+  
+
 
 export default App;
